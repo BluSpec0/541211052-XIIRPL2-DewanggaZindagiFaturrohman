ffcb064eeb777f2ab99e3c1de9439672074eeff8
Dewadewa <warasbanget76@gmail.com>
2023-11-01 20:30:45 +0700

    video 9

--- router/users.js
+++ router/users.js
@@ -1,34 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const users = [
-    {id: 1, nama: "Dewa", email: "warasbanget76@gmail.com"},
-    {id: 2, nama: "Fatur", email: "banget@gmail.com"}
-]
+const usercontroller = require('../controller/user')
 
-router.get('/users', (req, res) => {
-    res.json(users)
-  })
+router.get('/users', usercontroller.index)
 
-  router.post('/user', (req, res) => {
-    users.push(req.body)
-    res.json(users)
-  })
+  router.post('/user', usercontroller.store)
 
-  router.put('/user/:id', (req, res) => {
-    const id = req.params.id
-    users.filter(user => {
-        if(user.id == id){
-            user.nama = req.body.nama
-            user.email = req.body.email
-            return user
-        }
-    })
-    res.json(users)
-  })
+  router.put('/user/:id', usercontroller.update)
 
-  router.delete('/user/:id', (req, res) => {
-    res.send('Got a DELETE request at /user')
-  })    
+  router.delete('/user/:id', usercontroller.delete)    
 
 module.exports  = router
